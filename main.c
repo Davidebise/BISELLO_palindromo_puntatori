7535e039a9770c24699ccc75047760e3177579a9
Davide <12999297@itisrossi.vi.it>
2025-09-29 13:17:23 +0200

    Finished program

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 int main(void) {
-    printf("Hello, World!\n");
+    char stringa[]="i topi non avevano nipoti";
+    char *p1=stringa;
+    char *p2=stringa;
+    bool palindromo=true;
+
+    while (*p2!='\0')
+    {
+        p2++;
+    }
+    p2--;
+
+    while (p1<p2)
+    {
+        if (*p1==' ')
+            p1++;
+
+        if (*p2==' ')
+            p2--;
+
+        if (*p1!=*p2) {
+            palindromo=false;
+        }
+        p1++;
+        p2--;
+    }
+
+    if (palindromo) {
+        printf("palindromo\n");
+    }else {
+        printf("non palindromo\n");
+    }
     return 0;
 }
